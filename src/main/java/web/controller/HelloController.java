ae3231f412b27e44f94acf35ab97174aae5a376f
Mikhail <therapi5un@yandex.ru>
2023-06-03 02:09:28 +0500

    test

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,23 +1,14 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
-
-	@Autowired
-	private CarService carService;
-
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
